6c849ab87981867d2b9e58ee17a7acd124be3d56
Paul Campbell <paul.campbell@stonybrook.edu>
2014-04-24 20:12:24 -0400

    Added a nodejs server file and a configuration file

---
+++ config.js
@@ -0,0 +1,11 @@
+/* Configuration file for the monitor server */
+var config = {};
+
+/* To add config values do:
+ * config.sample = process.env.SAMPLE || 'DOIWORK';
+ *
+ */
+
+
+/* Export these configuration values */
+module.exports = config;
